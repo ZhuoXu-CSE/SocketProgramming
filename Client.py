19487aa3d44e8c2b957e98957495cf0fc9341e97
Z X <zx@Zs-MacBook-Pro.attlocal.net>
2020-04-03 15:30:52 -0400

    version2

--- Client.py
+++ Client.py
@@ -1,23 +1,32 @@
 import socket
 import sys
 
+BUF_SIZE = 1024
 if len(sys.argv) == 3:
     ServerIp = sys.argv[1]
     ServerPort = sys.argv[2]
 else:
     print("\n  Run the program like:\n python3 Client.py < serverip address > <Port number>\n")
     exit(1)
-
+commands = ["Upload", "Retrieve", "Sign in", "Sign out" "Logging", "List"]
 s = socket.socket()
 s.connect((ServerIp, int(ServerPort)))
-print(s.recv(1024).decode())
-Filename = input("\nPlease enter file name:\n")
-s.send(Filename.encode())
-Filename = "/Users/zx/Desktop/CSE3461Lab/Test/" + Filename
-file = open(Filename, "rb")
-SendData = file.read(1024)
-while SendData:
-    s.send(SendData)
-    SendData = file.read(1024)
-file.close()
-s.close()
+print(s.recv(BUF_SIZE).decode())
+while True:
+    while True:
+        Command = input("\nPlease enter command:\n Upload, Retrieve, Sign in, Sign out Logging, List\nCommand:")
+        s.send(Command.encode())
+        Data = s.recv(BUF_SIZE)
+        print(Data.decode())
+        if not Data:
+            break
+    Filename = input("\nPlease enter file name:\n")
+    s.send(Filename.encode())
+    Filename = "/Users/zx/Desktop/CSE3461Lab/Test/" + Filename
+    file = open(Filename, "rb")
+    SendData = file.read(BUF_SIZE)
+    while SendData:
+        s.send(SendData)
+        SendData = file.read(BUF_SIZE)
+    file.close()
+    s.close()
--- Server.py
+++ Server.py
@@ -10,24 +10,36 @@ else:
     print("\n Run the program like:\n python3 Server.py <Port number>\n")
     exit(1)
 
-commands = ["Upload", "Retrieve", "Sign in", "Sign out" "Logging", "List"]
+
+def upload(conection):
+    pass
+
+
+commands = ["Upload", "Retrieve", "Signin", "Signout" "Logging", "List"]
 s = socket.socket()
 s.bind(('', int(PORT)))
 s.listen()
 while True:
     conn, addr = s.accept()
-    msg = "\n\n|---------------------------------|\n Hi Client[IP address: " + addr[0] + \
-          "], \n ֲֳ**Welcome** \n -Server\n|---------------------------------|\n \n\n"
+    msg = "\nHi Client[IP address: " + addr[0] + "], \n ֲֳ**Welcome** \n -Server\n"
     conn.send(msg.encode())
+    Comm = conn.recv(BUF_SIZE)
+    args = Comm.decode().split()
+    while args[0] not in commands:
+        msg = "Invalid command: " + args[0] + "\n\nCommands:\n  Upload\n  Retrieve\n  Signin\n  Signout\n  Logging" \
+                                              "\n  List\n "
+        conn.send(msg.encode())
+        Comm = conn.recv(BUF_SIZE)
+        args = Comm.decode().split()
     # Receive filename from client side
-    Filename = conn.recv(1024)
+    Filename = conn.recv(BUF_SIZE)
     if Filename:
         filepath = "/Users/zx/Desktop/CSE3461Lab/Receive/" + Filename.decode()
         File = open(filepath, "wb")
-        RecvData = conn.recv(1024)
+        RecvData = conn.recv(BUF_SIZE)
         while RecvData:
             File.write(RecvData)
-            RecvData = conn.recv(1024)
+            RecvData = conn.recv(BUF_SIZE)
     File.close()
     print("\n File has been Transferred successfully \n")
     conn.close()
