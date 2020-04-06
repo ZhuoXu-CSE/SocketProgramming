2b5ab870c2a16be5a29d6d98fbe9fa84e2268b88
Z X <zx@Zs-MacBook-Pro.attlocal.net>
2020-04-06 00:13:42 -0400

    Socket programming

--- Client.py
+++ Client.py
@@ -1,8 +1,9 @@
 import socket
 import sys
+import time
 
 BUF_SIZE = 1024
-BASE_ADD = "/Users/zx/Desktop/CSE3461Lab/Test/"
+BASE_ADD = ""
 
 if len(sys.argv) == 3:
     ServerIp = sys.argv[1]
@@ -16,12 +17,9 @@ def upload(soc):
     filename = input("\nPlease enter file name:\n")
     soc.send(filename.encode())
     filename = BASE_ADD + filename
-    file = open(filename, "rb")
-    SendData = file.read(BUF_SIZE)
-    while SendData:
-        soc.send(SendData)
-        SendData = file.read(BUF_SIZE)
-    file.close()
+    with open(filename, "rb") as file:
+        soc.sendfile(file,0)
+    print("\n File has been Transferred successfully \n")
 
 
 def retrieve(soc):
@@ -29,14 +27,12 @@ def retrieve(soc):
     soc.send(filename.encode())
     if filename:
         filepath = BASE_ADD + filename
-        file = open(filepath, "wb")
-        recvdata = soc.recv(BUF_SIZE)
-        msg = recvdata.decode()
-        while recvdata and msg != "File not found":
-            file.write(recvdata)
+        with open(filepath, "wb") as file:
             recvdata = soc.recv(BUF_SIZE)
-        file.close()
-    print("\n File has been Transferred successfully \n")
+            while recvdata:
+                file.write(recvdata)
+                recvdata = soc.recv(BUF_SIZE)
+    print("\n File has been retrieved successfully \n")
 
 
 
@@ -47,36 +43,44 @@ print(s.recv(BUF_SIZE).decode())
 user = input("Please input Username and password: ")
 s.send(user.encode())
 info = s.recv(BUF_SIZE)
-if info.decode() == "Welcome":
+if info.decode() == "\n  Welcome":
     print(info.decode())
 else:
     while info.decode() == "User not found":
+        print(info.decode())
         user = input("Please input Username and password: ")
         s.send(user.encode())
         info = s.recv(BUF_SIZE)
-datatran = socket.socket()
-datatran.connect((ServerIp, int(ServerPort)+1))
+    print(info.decode())
 while True:
-    Command = input("\nPlease enter command:\n Upload, Retrieve, List, Logging Quit\nCommand:")
+    Command = input("\nPlease enter command:\n Upload, Retrieve, List, Logging, Quit\nCommand:")
     s.send(Command.encode())
     while Command:
         if Command in commands:
             break
         Data = s.recv(BUF_SIZE)
         print(Data.decode())
-        Command = input("\nPlease enter command:\n Upload, Retrieve, List, Logging Quit\nCommand:")
+        Command = input("\nPlease enter command:\n Upload, Retrieve, List, Logging, Quit\nCommand:")
         s.send(Command.encode())
     if Command == "Upload":
-        upload(datatran)
+        time.sleep(1)
+        soc = socket.socket()
+        soc.connect((ServerIp, int(ServerPort)+1))
+        upload(soc)
+        soc.close()
     elif Command == "Retrieve":
-        retrieve(datatran)
+        time.sleep(1)
+        soc = socket.socket()
+        soc.connect((ServerIp, int(ServerPort) + 1))
+        retrieve(soc)
+        soc.close()
     elif Command == "Logging":
-        log=datatran.recv(BUF_SIZE)
-        print(log.decode())
+        log = s.recv(BUF_SIZE)
+        print("\n" + log.decode())
     elif Command == "List":
-        msg = datatran.recv(BUF_SIZE)
-        print(msg.decode())
+        msg = s.recv(BUF_SIZE)
+        print("\n" + msg.decode())
     elif Command == "Quit":
+        print("\n Connection closed\n")
         break
 s.close()
-datatran.close()
--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ import os
 
 PORT = 0
 BUF_SIZE = 1024
-BASE_ADD = "/Users/zx/Desktop/CSE3461Lab/Receive/"
+BASE_ADD = ""
 if len(sys.argv) == 2:
     PORT = sys.argv[1]
     print("\n Server is listening on port :", PORT, "\n")
@@ -18,12 +18,11 @@ def upload(connection):
     filename = connection.recv(BUF_SIZE)
     if filename:
         filepath = BASE_ADD + filename.decode()
-        file = open(filepath, "wb")
-        recvdata = connection.recv(BUF_SIZE)
-        while recvdata:
-            file.write(recvdata)
+        with open(filepath, "wb") as file:
             recvdata = connection.recv(BUF_SIZE)
-        file.close()
+            while recvdata:
+                file.write(recvdata)
+                recvdata = connection.recv(BUF_SIZE)
     print("\n File has been Transferred successfully \n")
 
 
@@ -32,10 +31,7 @@ def retrieve(connection):
     filepath = BASE_ADD + filename.decode()
     try:
         with open(filepath, 'rb') as file:
-            SendData = file.read(BUF_SIZE)
-            while SendData:
-                connection.send(SendData)
-                SendData = file.read(BUF_SIZE)
+            connection.sendfile(file,0)
     except IOError:
         msg = "File not found"
         connection.send(msg.encode())
@@ -48,7 +44,7 @@ def authorization(user):
         with open(filepath, 'r') as file:
             record = file.readline()
             while record:
-                if user == record:
+                if user.split() == record.split():
                     ret = True
                 record = file.readline()
         return ret
@@ -61,30 +57,27 @@ commands = ["Upload", "Retrieve", "Logging", "List", "Quit"]
 s = socket.socket()
 s.bind(('', int(PORT)))
 s.listen()
-datatran = socket.socket()
-datatran.bind(('', int(PORT)+1))
-datatran.listen()
 while True:
     conn, addr = s.accept()
     log.append(addr[0])
-    msg = "\nHi Client[IP address: " + addr[0] + "], \n ֲֳ**Welcome** \n -Server\n"
+    msg = "\nHi Client[IP address: " + addr[0] + "],\n ===Welcome===\n -Server "
     conn.send(msg.encode())
     user = conn.recv(BUF_SIZE)
     auth = authorization(user.decode())
     if auth:
-        msg = "Welcome"
+        msg = "\n Welcome"
         conn.send(msg.encode())
     else:
         while not authorization(user.decode()):
             msg = "User not found"
             conn.send(msg.encode())
             user = conn.recv(BUF_SIZE)
+        msg = "\n Welcome"
+        conn.send(msg.encode())
     Comm = conn.recv(BUF_SIZE)
     log.append(Comm.decode())
     args = Comm.decode().split()
-    conndata, daddr = datatran.accept()
     while len(args) > 0:
-        #print(args)
         while args[0] not in commands or len(args) > 1:
             if args[0] not in commands:
                 msg = "Invalid command: " + args[0] + "\n\nCommands:\n  Upload\n  Retrieve\n  Logging\n  List\n  Quit\n"
@@ -99,23 +92,32 @@ while True:
                 log.append(Comm.decode())
                 args = Comm.decode().split()
         if args[0] == "Upload":
-            upload(conndata)
+            datatran = socket.socket()
+            datatran.bind(('', int(PORT) + 1))
+            datatran.listen()
+            data, addr = datatran.accept()
+            upload(data)
+            data.close()
         elif args[0] == "Retrieve":
-            retrieve(conndata)
+            datatran = socket.socket()
+            datatran.bind(('', int(PORT) + 1))
+            datatran.listen()
+            data, addr = datatran.accept()
+            retrieve(data)
+            data.close()
         elif args[0] == "Logging":
-            mg = ''.join(log)
-            conndata.send(mg.encode())
+            mg = '\n'.join(log)
+            conn.send(mg.encode())
         elif args[0] == "List":
             msg = os.listdir(BASE_ADD)
-            conndata.send(''.join(msg).encode())
+            conn.send('\n'.join(msg).encode())
         Comm = conn.recv(BUF_SIZE)
+        log.append(Comm.decode())
         args = Comm.decode().split()
-    #conn.close()
     print("\n Server closed the connection \n")
 
     # Come out from the infinite while loop as the file has been copied from client.
     break
 with open(BASE_ADD + "logging.txt", 'w') as file:
-    file.write(''.join(log))
+    file.write('\n'.join(log))
 s.close()
-datatran.close()
